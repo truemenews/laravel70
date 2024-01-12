94f46d3396c444614581363e5da506053ad01c96
quocvo87 <quocvo87@gmail.com>
2024-01-12 16:56:31 +0700

    ini broadcast

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('login', 'Auth\LoginController@login');
+Route::post('login', 'Auth\LoginController@login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('message/index', 'MessageController@index');
+Route::get('message/send', 'MessageController@send');
